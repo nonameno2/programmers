cb861ce8fa8c9fa48ad68a9a4efc260b24e17164
nonameno2 <nonameno1@protonmail.com>
2021-09-09 11:26:41 +0900

    [2_9_marathon.py]

---
+++ 2_9_marathon.py
@@ -0,0 +1,19 @@
+#1
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)  
+    return list(answer.keys())[0]
+
+
+#2
+def solution(participant, completion):
+
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+
+    return participant.pop()
